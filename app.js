ed93762458c252dc1588f693fd9f5c6a775c5c6f
ishan-bhuta02 <ishan.bhuta02@gmail.com>
2022-08-22 20:51:19 +0530

    add sample code

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello This a Node app sample!')
 })
 
+app.get('/get-user', (req, res) => {
+  res.send({
+    name: "ishan",
+    age: 32,
+    id: 1
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
